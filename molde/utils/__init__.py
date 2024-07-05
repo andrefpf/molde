b464a0d77483654dc8e12a31140b4fd8fecd7710
André Fernandes <fpf.andre@gmail.com>
2024-07-05 11:39:57 -0300

    update imports

--- molde/utils/__init__.py
+++ molde/utils/__init__.py
@@ -1,3 +1,3 @@
-from .poly_data_utils import *
+from .poly_data_utils import set_polydata_colors, set_polydata_property
 from .tree_info import TreeInfo
 from .format_sequences import format_long_sequence
