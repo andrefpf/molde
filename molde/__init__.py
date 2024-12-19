b84554a7377b472a1bcd3c108aac8d0644f345bb
R0drigoSchwartz <rodrigo32003@gmail.com>
2024-12-19 13:18:39 -0300

    Adds load_ui function

--- molde/__init__.py
+++ molde/__init__.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-
+from qtpy.uic import loadUi
 from .colors import Color
 
 MOLDE_DIR = Path(__file__).parent
+
+def load_ui(uifile, baseinstance):
+    return loadUi(uifile, baseinstance)
