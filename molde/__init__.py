4ff2249845a12ba52e6616556750abcee2ba387f
R0drigoSchwartz <rodrigo32003@gmail.com>
2025-01-28 13:07:21 -0300

    Small adjustment in load_ui function parameters

--- molde/__init__.py
+++ molde/__init__.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from qtpy.uic import loadUi
 from .colors import Color
+from PySide6.QtCore import QDir
 
 MOLDE_DIR = Path(__file__).parent
 
-def load_ui(uifile, baseinstance, icons_dir=None):
-    return loadUi(uifile, baseinstance, icons_dir)
+def load_ui(uifile: str | Path, baseinstance, working_directory: str | Path = None):
+    return loadUi(uifile, baseinstance, QDir(working_directory))
