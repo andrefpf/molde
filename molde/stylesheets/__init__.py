6b78cee702f622892927b286111157720375f3d3
R0drigoSchwartz <rodrigo32003@gmail.com>
2025-02-25 14:40:27 -0300

    Changes light theme border-color to make some tests in vibra

--- molde/stylesheets/__init__.py
+++ molde/stylesheets/__init__.py
@@ -35,7 +35,7 @@ def get_variables(theme:Literal["light", "dark"] = "light") -> dict:
             "@on-primary": color_names.WHITE.to_hex(),
             "@on-background": color_names.BLACK.to_hex(),
 
-            "@border-color": color_names.BLACK.to_hex(),
+            "@border-color": color_names.GRAY_7.to_hex(),
             "@input-color": color_names.GRAY_9.to_hex(),
 
             "@disabled-background": color_names.GRAY_8.to_hex(),
@@ -71,7 +71,7 @@ def get_variables(theme:Literal["light", "dark"] = "light") -> dict:
             "@on-background": color_names.WHITE.to_hex(),
             "@on-primary": color_names.WHITE.to_hex(),
 
-            "@border-color": color_names.GRAY_2.to_hex(),
+            "@border-color": color_names.WHITE.to_hex(),
             "@input-color": color_names.GRAY_3.to_hex(),
 
             "@disabled-background": color_names.GRAY_0.to_hex(),
